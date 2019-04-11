2a4ff0aceb472dc4b258d9ac7bb067b6ce56ab55
Gabriel Krell <gabrielkrell@gmail.com>
2019-04-10 21:35:08 -0500

    Rejigger for new pins; more robust errors

--- src/potnia-argon.cpp
+++ src/potnia-argon.cpp
@@ -6,51 +6,46 @@ void initializeSS();
 void loop();
 
 // **** Pin definitions ****
-int stemmaGNDPin = D3;
-int stemma3VPin = D2;
-int stemmaSCLPin = D1;
-int stemmaSDAPin = D0;
 
 // **** Moisture sensor ****
-Adafruit_seesaw ss;
-bool initialized;
+Adafruit_seesaw stemma;
+char* error;
 
 
 void setup() {
-  pinMode(D7, OUTPUT);
-  pinMode(stemmaGNDPin, OUTPUT);
-  pinMode(stemma3VPin, OUTPUT);
-  digitalWrite(stemmaGNDPin, LOW);
-  digitalWrite(stemma3VPin, HIGH);
-  pinMode(stemmaSCLPin, INPUT_PULLUP);
-  pinMode(stemmaSDAPin, INPUT_PULLUP);
+  pinMode(D7, OUTPUT); // hello works
+
 
   Serial.begin(9600);
-  initializeSS();
-}
 
-void initializeSS() {
-  if (!ss.begin(0x36)) {
-    Serial.println("Couldn't initialize.");
-    initialized = false;
-  } else {
-    initialized = true;
+  if (!stemma.begin(0x36)) {
+    error = "Couldn't initialize stemma!";
   }
 }
 
 void loop() {
+  if (error) {
+    digitalWrite(D7, HIGH);
+    delay(50);
+    digitalWrite(D7, LOW);
+    delay(50);
+    digitalWrite(D7, HIGH);
+    delay(50);
+    digitalWrite(D7, LOW);
+    delay(50);
+    Serial.println(error);
+    return;
+  }
+
+
   // "it lives" status blink for testing
   digitalWrite(D7, HIGH);
   delay(500);
   digitalWrite(D7, LOW);
   delay(500);
 
-  if (initialized) {
-    Serial.print("seesaw started! version: ");
-    Serial.println(ss.getVersion());
-    Serial.print("Temperature: "); Serial.print(ss.getTemp()); Serial.println("*C");
-    Serial.print("Capacitive: "); Serial.println(ss.touchRead(0));
-  } else {
-    initializeSS();
-  }
+  Serial.print("seesaw started! version: ");
+  Serial.println(stemma.getVersion());
+  Serial.print("Temperature: "); Serial.print(stemma.getTemp()); Serial.println("*C");
+  Serial.print("Capacitive: "); Serial.println(stemma.touchRead(0));
 }
